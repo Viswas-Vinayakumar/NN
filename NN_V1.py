7a218ef9e3b4599cb28ebe81b35f98f9427f1b44
Viswas Vinayakumar <viswasvinayakumar@Mac.fritz.box>
2026-05-20 18:30:13 +0200

    Simple forward pass added

--- NN_V1.py
+++ NN_V1.py
@@ -1,1 +1,16 @@
 import numpy as np 
+
+def relu(z):
+    return np.maximum(0, z)
+
+def softmax(z):
+    e = np.exp(z - np.max(z)
+    return e/e.sum()
+
+def forward(X, W1, b1, W2, b2):
+    z1 = w1@X + b1
+    a1 = relu(z1)
+    z2 = w2@a1 + b2
+    out = softmax(z2)
+    return (z1,a1,z2,out)
+

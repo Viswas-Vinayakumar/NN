307dff0a6bd74f52c32c819587c82077d5094242
Viswas Vinayakumar <viswasvinayakumar@Mac.fritz.box>
2026-05-20 18:31:59 +0200

    Bug fixes

--- NN_V1.py
+++ NN_V1.py
@@ -4,13 +4,13 @@ def relu(z):
     return np.maximum(0, z)
 
 def softmax(z):
-    e = np.exp(z - np.max(z)
+    e = np.exp(z - np.max(z))
     return e/e.sum()
 
 def forward(X, W1, b1, W2, b2):
-    z1 = w1@X + b1
+    z1 = W1@X + b1
     a1 = relu(z1)
-    z2 = w2@a1 + b2
+    z2 = W2@a1 + b2
     out = softmax(z2)
     return (z1,a1,z2,out)
 
